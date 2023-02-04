938bf2ebd4fabeb65fe0da574bffa2ce11ec7c79
MaDHouSe79 <madhouse1979@gmail.com>
2023-02-04 19:58:46 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,7 +12,7 @@ shared_scripts {
     '@PolyZone/CircleZone.lua',
     '@PolyZone/ComboZone.lua',
     '@qb-core/shared/locale.lua',
-    'locale/nl.lua', -- replace with desired language
+    'locales/en.lua', -- replace with desired language
     'config.lua'
 }
 
